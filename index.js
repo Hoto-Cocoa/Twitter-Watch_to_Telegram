b1194a605ea0361c5cfba4940bda00b31a1999e7
Hoto Cocoa <cocoa@hoto.us>
2019-09-29 18:45:40 +0900

    Rewrite

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const config = require('./config');
-const Twitter = require('twitter');
-const TelegramBot = require('node-telegram-bot-api');
+const Config = require('./config');
+const bot = new (require('node-telegram-bot-api'))(Config.Telegram.Token, { polling: true });
 
-const client = new Twitter(config.tweetConfig);
-const token = config.teleToken;
-const chatId = config.teleChatId;
-const bot = new TelegramBot(token, {polling: true});
-
-
-client.stream('statuses/filter', {follow: config.tweetUser},  function(stream) {
-	stream.on('data', function(tweet) {
-		console.log(tweet);
-		bot.sendMessage(chatId, "Tweet Posted : " + tweet.text);
-	});
-
-	stream.on('error', function(error) {
-		console.log(error);
-	});
+new (require('twitter'))(Config.Twitter).stream('statuses/filter', { follow: Config.Twitter.Users.join(', ') }, stream => {
+	stream.on('data', tweet => bot.sendMessage(Config.Telegram.ChatId, `${tweet.display_text_range ? tweet.text.substring(...tweet.display_text_range) : tweet.text} ― ${tweet.user.name}(@${tweet.user.screen_name})\n\n${new Date(Number(tweet.timestamp_ms)).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo', hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' })}\nhttps://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`));
+	stream.on('error', e => console.error(e));
 });
